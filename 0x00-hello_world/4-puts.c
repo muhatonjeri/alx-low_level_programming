007099f140e688e59758345119beae2489976ad0
muhatonjeri <newtonnjeri20@gmail.com>
2022-09-10 02:44:52 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+*
+*main print the string
+*
+*usingg the main function
+*
+*thre program prints Programming is like building a multilingual puzzle
+*
+Return : Always 0 (success)   meaning success
+*
+*
+*/
+int main (void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-rite a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
