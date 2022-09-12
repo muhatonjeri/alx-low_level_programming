abf60b90dd5c6ae61375a76b5e94cc61cf611071
muhatonjeri <newtonnjeri20@gmail.com>
2022-09-12 10:06:22 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+/**
+
+ * main - Entrypoint
+
+ *
+
+ * Return: Always 0 (success)
+
+ *
+
+ */
+
+int main(void)
+
+{
+
+	int letter;
+
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+
+		letter = tolower(letter);
+
+		putchar(letter);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.
+I sometimes suffer from insomnia. And when I cannot fall asleep, I play what I call the alphabet game 
