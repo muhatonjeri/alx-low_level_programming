3456a45790a018b3cb5ebaf2c5ccd7ac7d371cb4
muhatonjeri <newtonnjeri20@gmail.com>
2022-09-22 12:35:17 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,79 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_i - prints an integer
- * @i: integer to print
- *
- * Return: number of chars and digits printed
- */
-int print_i(va_list i)
+int main(void)
 {
-	int a[10];
-	int j, m, n, sum, count;
+  int n;
+  int a[5];
+  int *p;
 
-	n = va_arg(i, int);
-	count = 0;
-	m = 1000000000;
-	a[0] = n / m;
-	for (j = 1; j < 10; j++)
-	{
-		m /= 10;
-		a[j] = (n / m) % 10;
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		count++;
-		for (j = 0; j < 10; j++)
-			a[j] *= -1;
-	}
-	for (j = 0, sum = 0; j < 10; j++)
-	{
-		sum += a[j];
-		if (sum != 0 || j == 9)
-		{
-			_putchar('0' + a[j]);
-			count++;
-		}
-	}
-	return (count);
-}
-
-/**
- * print_d - prints a decimal
- * @d: decimal to print
- *
- * Return: number of chars and digits printed
- */
-int print_d(va_list d)
-{
-	int a[10];
-	int j, m, n, sum, count;
-	
-	n = va_arg(d, int);
-	count = 0;
-	m = 1000000000;
-	a[0] = n / m;
-	for (j = 1; j < 10; j++)
-	{
-		m /= 10;
-		a[j] = (n / m) % 10;
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		count++;
-		for (j = 0; j < 10; j++)
-			a[j] *= -1;
-	}
-	for (j = 0, sum = 0; j < 10; j++)
-	{
-		sum += a[j];
-		if (sum != 0 || j == 9)
-		{
-			_putchar('0' + a[j]);
-			count++;
-		}
-	}
-	return (count);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
